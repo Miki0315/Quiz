7e03b5747101c6c4ba5f43ad6fa87e44ceede9e8
清水美希 <mroru0315@gmail.com>
2015-06-14 11:53:43 +0900

    second commit

--- Quiz/QuizViewController.swift
+++ Quiz/QuizViewController.swift
@@ -10,74 +10,113 @@ import UIKit
 
 class QuizViewController: UIViewController {
     //出題数
-    var questionNumber:Int = 5
-    //何問正解したかの共有
-    var correntAnswer:Int = 0
-    //randomの共有
-    var random:Int = 0
-    //解いた問題数の共有
+    var questionNumber:Int = 10
+    
+    //現在の問題数
     var sum:Int = 0
-    //Quiz配列
-    var quiz=[NSArray]()
-    //weak参照
-    @IBOutlet weak var QuizText: UITextView!
+    
+    //正解数
+    var correctAnswer:Int = 0
+    
+    //乱数
+    var random:Int = 0
+    
+    //クイズを格納する配列
+    var quizArray = [NSMutableArray]()
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons: Array<UIButton>!
 
+    //正解、不正解のラベル
+    @IBOutlet var label1:UILabel!
+    @IBOutlet var label2:UILabel!
+    
+    //count
+    var count:Int = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //------------------------ここから下にクイズを書く------------------------//
-        quiz.append(["問題文","選択肢","選択肢2","選択肢3",1])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        
-        //------------------------ここから下にクイズを書く------------------------//
+        quizArray.append(["夜中寝ないことの意味を持つ単語は？","オール","テール","アール",1])
+        quizArray.append(["バイブスの意味は？","ケータイのバイブ音","テンション","バイトの時間",2])
+        quizArray.append(["仕事や学校が終わるとすぐに家に変える人たちのことを表す単語は？","GHQ","PIQ","KFC",1])
+        quizArray.append(["「oc」の意味は？","おっちょこちょい","おいしい","お家",2])
+        quizArray.append(["「今日の髪型、与謝野ってるんだよね」この髪型はどんな状態か？","結んでいる","整っている","乱れている",3])
+        quizArray.append(["「あいつのメール幕の内なんだよねー。」これはどんなメールか？","内容が多く分かりにくい","ご飯の誘い","内容が少なく分かりやすい",1])
+        quizArray.append(["とにかく急ぐことを何というか？","Aダッシュ","Bダッシュ","Cダッシュ",2])
+        quizArray.append(["恋愛に対して積極的に見えて、実際は奥手な男性のことを何というか？","肉詰めピーマン系男子","ロールキャベツ系男子","アスパラベーコン巻き系男子",3])
+        quizArray.append(["「ウーロン茶」という人の特徴を表す単語の意味は？","お茶ばかり飲む人","うざい茶髪のロン毛の人","肌が茶色い人",2])
+        quizArray.append(["「JK2」の意味は？","JR快速2分遅れ","女性看護婦20代","女子高校2年生",3])
+        quizArray.append(["「いもる」の意味とは？","芋を食べる","緊張してオドオドする","胃がもたつく",2])
 
-        random = Int(arc4random()%UInt32(quiz.count))
-        QuizText.text=quiz[random][0] as NSString
+        //------------------------ここから下にクイズを書く------------------------//
+        choiceQuiz()
         
+          }
+    
+    func choiceQuiz() {
+        println(quizArray.count)
+        //クイズの問題文をシャッフルしてTextViewにセット
+        random = Int(arc4random_uniform(UInt32(quizArray.count)))
+        quizTextView.text = quizArray[random][0] as! NSString as String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        for var i = 0; i < choiceButtons.count; i++ {
+            choiceButtons[i].setTitle(quizArray[random][i+1] as! NSString as String, forState: .Normal)
+            
+            //どのボタンが押されたか判別するためのtagをセット
+            choiceButtons[i].tag = i + 1;
+        }
     }
-    @IBAction func Select1() {
+    @IBAction func choiceAnswer(sender: UIButton) {
         sum++
-        if 1==quiz[random][4] as NSObject{
-            current()
+        println("random \(random)")
+        if quizArray[random][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer++
+            if count == 0{
+            label1.text = String("正解")
+            }
+        }else{
+            if count == 0{
+            label2.text = String("不正解")
+            }
         }
-        if(sum == questionNumber){
-            tapBtn()
+        
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if sum == questionNumber {
+            performSegueToResult()
         }
+        quizArray.removeAtIndex(random)
+        choiceQuiz()
     }
-    @IBAction func Select2() {
-        sum++
-        if 2==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    @IBAction func Select3() {
-        sum++
-        if 3==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    func current(){
-        println("true")
-        correntAnswer++
-    }
-    func tapBtn() {
+    
+    func performSegueToResult() {
         performSegueWithIdentifier("toResultView", sender: nil)
     }
-/*
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toResultView") {
+            
+            var ResultView : ResultViewController = segue.destinationViewController as! ResultViewController
+
+            ResultView.correctAnswer = self.correctAnswer
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-*/
+    
 }
+
+
+
+
--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -8,28 +8,85 @@
 
 import UIKit
 
+//背景
+//@IBOutlet weak var myImageView: UIImageView!
+//var myImageView: UIImageView!
+
 class ResultViewController: UIViewController {
+    
+    @IBOutlet var label3:UILabel!
+    @IBOutlet var label4:UILabel!
+    @IBOutlet var label5:UILabel!
+
+    @IBOutlet var myImageView: UIImageView!
+     var correctAnswer:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if correctAnswer == 10{
+            label3.text = String("10問中10問正解しました！！！")
+            //label4.textcolor = UIcolor.redColor()
+            label4.text = String("100％")
+            label5.text = String("あなたは若者界の神様です")
+            myImageView.image = UIImage(named:"tengoku.jpg")
+        }else if correctAnswer == 9{
+            label3.text = String("10問中9問正解しました！！")
+            label4.text = String("90％")
+            label5.text = String("あなたはシティーボーイ！")
+            myImageView.image = UIImage(named:"tokai.jpg")
+        }else if correctAnswer == 8{
+            label3.text = String("10問中8問正解しました。")
+            label4.text = String("80％")
+            label5.text = String("若者の憧れ！")
+            myImageView.image = UIImage(named: "sibuya.jpg")
+        }else if correctAnswer == 7{
+            label3.text = String("10問中7問正解しました。")
+            label4.text = String("70％")
+            label5.text = String("もうひといき！")
+            myImageView.image = UIImage(named: "town.jpg")
+        }else if correctAnswer == 6{
+            label3.text = String("10問中6問正解しました。")
+            label4.text = String("60％ ")
+            label5.text = String("まあまあですね")
+            myImageView.image = UIImage(named: "sitamati.jpg")
+        }else if correctAnswer == 5{
+            label3.text = String("10問中5問正解しました。")
+            label4.text = String("50％")
+            label5.text = String("可もなく不可もなく")
+            myImageView.image = UIImage(named: "farm.jpg")
+        }else if correctAnswer == 4{
+            label3.text = String("10問中4問正解しました。")
+            label4.text = String("40％")
+            label5.text = String("若者言葉って難しいですね‥")
+            myImageView.image = UIImage(named: "inaka1.jpg")
+        }else if correctAnswer == 3{
+            label3.text = String("10問中3問正解しました。")
+            label4.text = String("30％")
+            label5.text = String("がんばりましょう‥")
+            myImageView.image = UIImage(named: "inaka2.jpg")
+        }else if correctAnswer == 2{
+            label3.text = String("10問中2問正解しました。")
+            label4.text = String("20％")
+            label5.text = String("まぐれの正解ですか？")
+            myImageView.image = UIImage(named: "inaka3.jpg")
+        }else if correctAnswer == 1{
+            label3.text = String("10問中1問正解しました。")
+            label4.text = String("10％")
+            label5.text = String("おつかれさまです")
+            myImageView.image = UIImage(named: "siba.jpg")
+        }else if correctAnswer == 0{
+            label3.text = String("10問中0問正解しました。")
+            label4.text = String("0％")
+            label5.text = String("時代に追いつきましょう")
+            myImageView.image = UIImage(named: "tuti.jpg")
         // Do any additional setup after loading the view.
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
